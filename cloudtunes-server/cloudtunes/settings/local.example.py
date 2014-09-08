06d7d9a6f5121c563998d10514837dc9418152c6
Jakub Roztocil <jakub@roztocil.name>
2014-09-08 15:56:34 +0200

    Updated local.example.py.

--- cloudtunes-server/cloudtunes/settings/local.example.py
+++ cloudtunes-server/cloudtunes/settings/local.example.py
@@ -5,31 +5,25 @@ from .defaults import *
 
 DEBUG = False
 
-FACEBOOK_APP_ID = None
-FACEBOOK_APP_SECRET = None
-
-
-LASTFM_API_KEY = None
-LASTFM_API_SECRET = None
-
-DROPBOX_API_APP_KEY = None
-DROPBOX_API_APP_SECRET = None
 
 MONGODB = {
     'host': 'localhost'
 }
+
+
 REDIS = {
     'host': 'localhost'
 }
-WEB_APP_DIR = path.realpath(path.join(
-    ROOT, '..',
-    'spa', 'build', 'development'
-))
+
+
 TORNADO_APP.update({
     'static_path': WEB_APP_DIR,
     'debug': DEBUG,
     'cookie_secret': None
 })
+
+
+WEB_APP_DIR = path.realpath(ROOT + '/../cloudtunes-webapp/build/development')
 
 
 EMAIL = {
@@ -38,3 +32,24 @@ EMAIL = {
     'SENDER': None,
     'PASSWORD': None
 }
+
+#############################################################
+# Facebook <https://developers.facebook.com/apps>
+#############################################################
+
+FACEBOOK_APP_ID = None
+FACEBOOK_APP_SECRET = None
+
+#############################################################
+# Last.fm <http://www.last.fm/api/account>
+#############################################################
+
+LASTFM_API_KEY = None
+LASTFM_API_SECRET = None
+
+#############################################################
+# Dropbox <https://www.dropbox.com/developers>
+#############################################################
+
+DROPBOX_API_APP_KEY = None
+DROPBOX_API_APP_SECRET = None
