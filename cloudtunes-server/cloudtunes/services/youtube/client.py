dc5f879f0f066624b51b78a180cf3c9a597cd3cb
hardcoder80 <quick.agent80@gmail.com>
2014-09-19 11:33:43 +0000

    handle redirects through youtube shorteners

--- cloudtunes-server/cloudtunes/services/youtube/client.py
+++ cloudtunes-server/cloudtunes/services/youtube/client.py
@@ -13,12 +13,25 @@ VIDEO_URL = 'https://gdata.youtube.com/feeds/api/videos/%s?v=2&alt=jsonc'
 
 def get_video_id(url):
     url = urlparse.urlparse(url)
-    if 'youtube.com' not in url.netloc:
-        raise ValueError('Not a youtube.com URL: %s' % url)
-    params = urlparse.parse_qs(url.query)
-    if 'v' not in params:
-        raise ValueError('Missing video ID: %s' % url)
-    return params['v'][0]
+
+    if 'youtube.com' in url.netloc:
+        params = urlparse.parse_qs(url.query)
+        if 'v' in params:
+            return params['v'][0]
+        else:
+            raise ValueError('Missing video ID: %s' % url)
+
+    elif 'youtu.be' in url.netloc:
+        if len(url[2]) > 1:
+           return url[2].split('/')[1]
+        else:
+            raise ValueError('Missing video ID: %s' % url)
+
+    else:
+        raise ValueError('Not a youtube.com or youtu.be URL: %s' % url)
+
+
+
 
 
 def get_video(id):
